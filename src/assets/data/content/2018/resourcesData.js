0e8241c0a84ddf25f64ac3f9aa43939e28fbd2cd
Po Chun, Lu <oceanus11034@gmail.com>
2018-02-25 13:38:43 +0800

    [Update] resources text

--- src/assets/data/content/2018/resourcesData.js
+++ src/assets/data/content/2018/resourcesData.js
@@ -1,6 +1,6 @@
 export default `
   Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, error. Quos nobis minima veritatis officia provident, repudiandae
   omnis neque porro impedit delectus quis corrupti quidem iste minus quia aliquam quasi.
-  - text
-  - text
+  - markdown list
+  - markdown list
 `;
