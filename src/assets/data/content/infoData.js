782aaa206132f4a0f6009c8990dbbb00a66b9f31
Po Chun, Lu <oceanus11034@gmail.com>
2019-02-18 21:55:01 +0800

    temp set tbd

--- src/assets/data/content/infoData.js
+++ src/assets/data/content/infoData.js
@@ -19,12 +19,7 @@ export default {
   }],
   title: 'Data Science and Artificial Intelligence Practice',
   description:
-    `Data Mining/Machine Learning Theory and Practice. 
-    In this course, a series of DM/ML research topics will be taught. 
-    There are some short technical lectures and short academic/industrial panel discussions. 
-    We emphasize the importance of practices in this course. 
-    Your brain, theoretically, is a kind of models which should be trained by lots of instances. 
-    Based on the spirit of trial-and-error, we will give 4 HWs, 2 projects, one paper presentation, and mostly, you should program individually.`,
+    `TBD`,
   teachers: {
     instructors: [
       {
@@ -64,11 +59,12 @@ export default {
     },
     {
       title: 'Grading',
-      content: `- 4 HWs (50%)<br>
-      - Mid-term project (15%)<br>
-      - Team-based paper presentation (10%)<br>
-      - Final project (25%)<br>
-      - Panel discussion (5% bonus)`,
+      // content: `- 4 HWs (50%)<br>
+      // - Mid-term project (15%)<br>
+      // - Team-based paper presentation (10%)<br>
+      // - Final project (25%)<br>
+      // - Panel discussion (5% bonus)`,
+      content: `TBD`,
       icon: `
         <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8">
           <path d="M3.5 0c-.97 0-1.84.4-2.47 1.03l2.97 2.97v-3.97c-.16-.02-.33-.03-.5-.03zm1.5 1.06v3.41l-2.72 2.72c.61.5 1.37.81 2.22.81 1.93 0 3.5-1.57 3.5-3.5 0-1.76-1.31-3.19-3-3.44zm-4.09 1.31c-.56.54-.91 1.29-.91 2.13 0 .96.46 1.79 1.16 2.34l2.13-2.13-2.38-2.34z"/>
